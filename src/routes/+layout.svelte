4699920e3cf347113d99b6184ca1f1118de822dc
artificial <59352535+artificialbutter@users.noreply.github.com>
2024-04-01 15:14:42 -0700

    analytics!

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,14 @@
     import { dev } from '$app/environment';
 
     inject({ mode: dev ? 'development' : 'production' });
+
+    onMount(() => {
+        const analyticsScript = document.createElement('script');
+        analyticsScript.defer = true;
+        analyticsScript.dataset.domain = 'gaycat.online';
+        analyticsScript.src = 'https://a.arti.lol/js/script.js';
+        document.head.appendChild(analyticsScript);
+    });
 </script>
 
 <svelte:head>
