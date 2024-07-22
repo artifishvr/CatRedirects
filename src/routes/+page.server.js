bef21cbc27f880ce85c53bdfb7719963ee593f08
Arti <59352535+artifishvr@users.noreply.github.com>
2024-07-21 18:05:48 -0700

    homepage improvements

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,10 +1,12 @@
 import { kindeAuthClient } from '@kinde-oss/kinde-auth-sveltekit';
+import { redirect } from '@sveltejs/kit';
 
 export async function load({ request }) {
     const isAuthenticated = await kindeAuthClient.isAuthenticated(
         request
     );
 
+    if (isAuthenticated) redirect(302, '/dash');
 
     return {
         isAuthenticated
